01168d745693f3a33202e97c69c3ab58bc9950d2
Tripura kant <tripurakant@gmail.com>
2023-05-21 11:00:35 +0530

    2

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-project_id = "REPLACE_ME"
+project_id = "molten-enigma-386006"
 region     = "us-central1"
--- vpc.tf
+++ vpc.tf
@@ -16,13 +16,13 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "${var.project_id}-vpc1"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "${var.project_id}-subnet1"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
