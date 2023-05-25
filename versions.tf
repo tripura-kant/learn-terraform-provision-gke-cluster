40ca806f94d377931149759c1241aa1d8f76902d
Tripura kant <tripurakant@gmail.com>
2023-05-25 20:55:00 +0530

    asdd

--- versions.tf
+++ versions.tf
@@ -9,3 +9,10 @@ terraform {
   required_version = ">= 0.14"
 }
 
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  credentials = file("F:/Repo-all/terraform/fiverr/New folder/molten-enigma-386006-2afa6ef7914c.json")
+  zone = var.zone
+}
+
--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  credentials = file("F:/Repo-all/terraform/fiverr/New folder/molten-enigma-386006-2afa6ef7914c.json")
-  zone = var.zone
-}
-
-
-# provider "google" {
-#   credentials = file("F:/Repo-all/terraform/fiverr/New folder/molten-enigma-386006-2afa6ef7914c.json")
-#   project     = "your-project-id"
-#   region      = "us-central1"
-#   zone        = "us-central1-a"
-# }
 
 # VPC
 data "google_compute_network" "vpc" {
